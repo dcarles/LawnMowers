e68f68ac636687ddca99cf4e84d74b8cac0b2186
danielcarles <danielcarles@gmail.com>
2015-10-28 18:16:17 +0000

    initial files

---
+++ LawnMowers/LawnMower.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawnMowers
+{
+    public class LawnMower
+    {
+
+        public enum Direction { N, E, S, W }
+        public enum Command { L, R, M }
+
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+     
+        public Direction Heading { get; private set; }
+
+        public LawnMower(int x, int y,  Direction heading)
+        {
+            Heading = heading;
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(Command command)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void ChangeDirection(Command command)
+        {
+            throw new NotImplementedException();
+        }
+       
+
+    }
+}
---
+++ LawnMowers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawnMowers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string filename = "sample";
+            string filename = "sample";
+            //string filename = "B-large";
+            using (StreamReader reader = new StreamReader("..\\..\\" + filename + ".in"))
+            {
+                using (StreamWriter writer = new StreamWriter("..\\..\\" + filename + ".out"))
+                {
+                    int fieldSize = Int32.Parse(reader.ReadLine());
+
+                    LawnMower mower = new LawnMower();
+                       
+                    }
+                }
+            }
+        }
+    
+}
